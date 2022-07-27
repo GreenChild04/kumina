0bd2f1d528f5aefe9cad83fec734c85c1db0cca2
GreenChild04 <greenchild04@protonmail.com>
2022-07-27 17:59:26 -0400

    udrs: I have started work on the script object
    I have started work on the script object in udrscript

--- Kumina/udr/udrScript.py
+++ Kumina/udr/udrScript.py
@@ -1,5 +1,7 @@
 from utils.cmdUtils.userKeyUtils import UserKeyUtils
+from menus.productLock import ProductLock
 from dataclasses import dataclass
+import pickle
 
 
 class UdrScript:
@@ -10,23 +12,25 @@ class UdrScript:
     def run(self):
         if self.script[0] == "#! /udr/script":
             self.script = self.script[1:]
-            for i in self.script:
-                print(i)
+            USC(self.script)
         else:
             pass
 
 
-dataclass()
+@dataclass()
 class USO:
     script: list
     user: str
     pwd: str
     act: str = None
 
-    def createAct(self):
+    def runScript(self):
         pass
 
 
 class USC:
     def __init__(self, script):
-        pass
+        self.script = script
+
+    def compile(self):
+        uso = USO(self.script, )
